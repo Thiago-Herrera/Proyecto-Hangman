a6e84d228c32b068a0387cb8ebaf6da0eb5e473e
KevinrealG <kevinsossav@gmail.com>
2025-04-07 08:42:40 -0500

    .

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
 
 
 
-
+Dificultad = "dificil" # variable global
 
 class Ahorcado:
-    def __init__(self, Dificultad = "Normal", intentos_maximos = 20): #<- parametro
+    """
+    Clase Ahorcado que representa el juego del ahorcado."""
+    def __init__(self, intentos_maximos = 20): #<- parametro
+        Dificultad = "facil"
         self.Nombre_Jugador = "" 
         self.Nombre_Ahocado = ""
-        self.Dificultad = Dificultad
+        self.Dificultad = Dificultad # variable local
         self.intentos_maximos =  intentos_maximos
         self.Palabra_secreta = ""
         self.letras_usadas = []
         self.letras_adivinadas = []
-Ahorcado()
+
+ahorcado = Ahorcado() # Instancia de la clase Ahorcado
+print(ahorcado.Dificultad) # Imprime la dificultad por defecto
+print(Dificultad) # Imprime la dificultad global
+print(ahorcado.__doc__)
